0d74979aab5a8b822004f118ba22661dac3711c7
misha <mihalious@proton.me>
2024-03-21 01:40:19 +0200

    implement eating with mutex

--- src/main.c
+++ src/main.c
@@ -4,28 +4,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-typedef struct Philosopher {
-    int32_t id;
-    bool lfork;
-    bool rfork;
-} Philosopher;
-
 typedef struct Fork {
-    int32_t id;
+    uint32_t id;
     bool in_use;
 } Fork;
-
+Fork fork_new(uint32_t id, bool in_use) {
+    Fork f = {id, in_use};
+    printf("Fork: id: %d, in_use: %d\n", f.id, f.in_use);
+    return f;
+}
 typedef struct ForksArray {
     Fork *ptr;
     size_t len;
 } ForksArray;
 
-typedef struct ThreadArgs {
-    Philosopher *philosopher;
-    ForksArray forks;
-} ThreadArgs;
+typedef struct Philosopher {
+    uint32_t id;
+    bool lfork;
+    bool rfork;
+} Philosopher;
 
-Philosopher philosopher_new(int32_t id, bool lfork, bool rfork) {
+Philosopher philosopher_new(uint32_t id, bool lfork, bool rfork) {
     Philosopher p = {id, lfork, rfork};
     printf("Philosopher: id: %d, lfork: %d, rfork: %d\n", p.id, p.lfork,
            p.rfork);
@@ -60,26 +59,61 @@ void philosopher_think(Philosopher *philosopher, uint64_t time) {
     sleep(time);
 }
 void phisosopher_eat(Philosopher *philosopher, ForksArray forks,
-                     uint64_t time) {
+                     pthread_mutex_t *mutex, uint64_t time) {
+    pthread_mutex_lock(mutex);
+
+    philosopher_try_rfork(philosopher, forks);
+    philosopher_try_lfork(philosopher, forks);
+
+    if (!(philosopher->lfork && philosopher->rfork)) {
+        printf("Philosopher %d couldn't eat\n", philosopher->id);
+
+        philosopher_release_lfork(philosopher, forks);
+        philosopher_release_rfork(philosopher, forks);
+
+        pthread_mutex_unlock(mutex);
+        return;
+    }
+    pthread_mutex_unlock(mutex);
+
+    if (philosopher->lfork && philosopher->rfork) {
+        printf("Philosopher %d is eating\n", philosopher->id);
+        sleep(time);
+    }
+
+    pthread_mutex_lock(mutex);
+
+    philosopher_release_lfork(philosopher, forks);
+    philosopher_release_rfork(philosopher, forks);
+
+    pthread_mutex_unlock(mutex);
 }
-void *philosopher_run(void *data) {
+
+typedef struct ThreadArgs {
+    ForksArray forks;
+    Philosopher *philosopher;
+    pthread_mutex_t mutex;
+} ThreadArgs;
+ThreadArgs thread_args_new(ForksArray forks, Philosopher *philosopher,
+                           pthread_mutex_t mutex) {
+    ThreadArgs t = {forks, philosopher, mutex};
+    return t;
+}
+
+void *thread_run(void *data) {
     ThreadArgs *args = (ThreadArgs *)data;
+
     Philosopher *philosopher = args->philosopher;
     ForksArray forks = args->forks;
+    pthread_mutex_t mutex = args->mutex;
     printf("Thread for Philosopher with id: %d\n", philosopher->id);
 
     while (true) {
         // time in seconds
-        uint64_t time = 3;
+        uint64_t time = 1;
         philosopher_think(philosopher, time);
-        phisosopher_eat(philosopher, forks, time * 2);
+        phisosopher_eat(philosopher, forks, &mutex, time * 2);
     }
-}
-
-Fork fork_new(int32_t id, bool in_use) {
-    Fork f = {id, in_use};
-    printf("Fork: id: %d, in_use: %d\n", f.id, f.in_use);
-    return f;
 }
 
 int main() {
@@ -102,21 +136,26 @@ int main() {
     }
     ForksArray forks = {.ptr = forks_ptr, .len = N};
 
-    pthread_t *handles = malloc(sizeof(pthread_t) * N);
-    if (!handles) {
-        return 1;
-    }
     ThreadArgs *thread_args = malloc(sizeof(ThreadArgs) * N);
     if (!thread_args) {
         return 1;
     }
     for (size_t i = 0; i < N; i++) {
-        thread_args[i].philosopher = &philosophers[i];
-        thread_args[i].forks = forks;
+        // thread_args[i].forks = forks;
+        // thread_args[i].philosopher = &philosophers[i];
+        // pthread_mutex_init(&thread_args[i].mutex, nullptr);
+        pthread_mutex_t mutex;
+        pthread_mutex_init(&mutex, nullptr);
+        thread_args[i] = thread_args_new(forks, &philosophers[i], mutex);
     }
 
+    pthread_t *handles = malloc(sizeof(pthread_t) * N);
+    if (!handles) {
+        return 1;
+    }
+    // spawning the treads
     for (size_t i = 0; i < N; i++) {
-        pthread_create(&handles[i], nullptr, philosopher_run,
+        pthread_create(&handles[i], nullptr, thread_run,
                        (void *)&thread_args[i]);
     }
     for (size_t i = 0; i < N; i++) {
